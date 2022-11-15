51b7ea1416a2d01091bdb6079b6e219703bdd190
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-11-15 17:03:45 -0300

    Add debugging info

--- prepare-composer-wp.sh
+++ prepare-composer-wp.sh
@@ -3,11 +3,13 @@ TARGET_DIR="${GITHUB_WORKSPACE}/${TARGET_DIR}"
 SOURCE_DIR="${GITHUB_WORKSPACE}/${SOURCE_DIR}"
 
 if [ ! -f "${TARGET_DIR}/composer.json" ]; then
+	echo "Not going to install anything with composer"
 	# Nothing to do here
 	exit 0;
 fi
 
 if [ ! -f "${SOURCE_DIR}/vendor/composer/installed.json" ]; then
+	echo "We don't have previously installed dependencies"
 	# Nothing to do here either
 	exit 0;
 fi
@@ -34,6 +36,7 @@ mv "${TARGET_DIR}/vendor/composer/installed_new.json" "${TARGET_DIR}/vendor/comp
 while IFS=\= read PACKAGE; do
 	PACKAGE_DIR="${PACKAGE#"../../"}"
 	PACKAGE_PARENT_DIR="$(dirname "$PACKAGE_DIR")"
+	echo "Attempting to restore ${PACKAGE_DIR}"
 	mkdir -p "${TARGET_DIR:?}/${PACKAGE_PARENT_DIR}"
 	rm -rf "${TARGET_DIR:?}/${PACKAGE_DIR}"
 	cp -rf "${SOURCE_DIR:?}/${PACKAGE_DIR}" "${TARGET_DIR:?}/${PACKAGE_DIR}"
