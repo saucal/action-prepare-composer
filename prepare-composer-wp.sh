736a593bf952f4a0705ae5907363665c3d6f4fc9
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-11-15 13:12:23 -0300

    Initial version for prepare

---
+++ prepare-composer-wp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+TARGET_DIR="${GITHUB_WORKSPACE}/${TARGET_DIR}"
+SOURCE_DIR="${GITHUB_WORKSPACE}/${SOURCE_DIR}"
+
+if [ ! -f "${TARGET_DIR}/composer.json" ]; then
+	# Nothing to do here
+	exit 0;
+fi
+
+if [ ! -f "${SOURCE_DIR}/vendor/composer/installed.json" ]; then
+	# Nothing to do here either
+	exit 0;
+fi
+
+
+
+# initialize composer on build dir
+mkdir -p "${TARGET_DIR}/vendor"
+rm -rf "${TARGET_DIR}/vendor/composer"
+cp -rf "${SOURCE_DIR}/vendor/composer" "${TARGET_DIR}/vendor/composer"
+
+# build list of plugins to be installed
+WHITELIST=$(jq -crM '[ .packages[] | .name ] + ["composer/installers"] | unique' "${TARGET_DIR}/composer.lock")
+
+# refresh installed.json based on packages previously installed
+{
+	jq --indent 4 -rM --argjson whitelist "${WHITELIST}" 'del(.packages[] | select( .name as $in | $whitelist | index($in) | not))' "${TARGET_DIR}/vendor/composer/installed.json"
+} > "${TARGET_DIR}/vendor/composer/installed_new.json"
+
+rm -rf "${TARGET_DIR}/vendor/composer/installed.json"
+mv "${TARGET_DIR}/vendor/composer/installed_new.json" "${TARGET_DIR}/vendor/composer/installed.json"
+
+# move files from location to new one
+while IFS=\= read PACKAGE; do
+	PACKAGE_DIR="${PACKAGE#"../../"}"
+	PACKAGE_PARENT_DIR="$(dirname "$PACKAGE_DIR")"
+	mkdir -p "${TARGET_DIR:?}/${PACKAGE_PARENT_DIR}"
+	rm -rf "${TARGET_DIR:?}/${PACKAGE_DIR}"
+	cp -rf "${SOURCE_DIR:?}/${PACKAGE_DIR}" "${TARGET_DIR:?}/${PACKAGE_DIR}"
+	# TODO: ^ move, instead of copy
+done < <(jq -crM 'del(.packages[] | select( .name as $in | ["composer/installers"] | index($in))) | .packages[]."install-path"' "${TARGET_DIR}/vendor/composer/installed.json")
+unset IFS
