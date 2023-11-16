e6c14757ed2beb3bd229ef7d7c7040141a683960
Kostas Seresiotis <kostas@saucal.com>
2023-11-17 00:46:27 +0200

    ahh

--- prepare-wp-core-update.sh
+++ prepare-wp-core-update.sh
@@ -23,7 +23,7 @@ fi
 echo "CORE_VERSION_COMPOSER_TO: $CORE_VERSION_COMPOSER_TO"
 echo "$CORE_VERSION_COMPOSER_TO" > "${RUNNER_TEMP}/core-version-composer-to"
 
-if [ -z "$CORE_VERSION_COMPOSER_FROM" ]; then
+if [ ! -z "$CORE_VERSION_COMPOSER_FROM" ]; then
     echo "CORE_VERSION_COMPOSER_FROM: $CORE_VERSION_COMPOSER_FROM"
     echo "$CORE_VERSION_COMPOSER_FROM" > "${RUNNER_TEMP}/core-version-composer-from"
 fi
