e30c89b8397a3f4b759d5e9e6a72376ed3c09438
Kostas Seresiotis <kostas@saucal.com>
2024-01-11 22:12:41 +0200

    on exit code, leave var empty

--- prepare-wp-core-update.sh
+++ prepare-wp-core-update.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 
 cd "${GITHUB_WORKSPACE}/${PATH_DIR}" || exit 1;
 
 cp "$GITHUB_ACTION_PATH/handle-wp-core-update.sh" "${RUNNER_TEMP}/handle-wp-core-update.sh"
 
-CORE_VERSION_COMPOSER_TO=$(composer config extra.wordpress-core)
+CORE_VERSION_COMPOSER_TO=$(composer config extra.wordpress-core) || CORE_VERSION_COMPOSER_TO=""
 
 cd "${GITHUB_WORKSPACE}/${FROM_DIR}" || exit 1;
 
-CORE_VERSION_COMPOSER_FROM=$(composer config extra.wordpress-core)
+CORE_VERSION_COMPOSER_FROM=$(composer config extra.wordpress-core) || CORE_VERSION_COMPOSER_FROM=""
 
 if [ -z "$CORE_VERSION_COMPOSER_TO" ]; then
     echo "CORE_VERSION_COMPOSER_TO is not set."
